a5143a7ca0a66735ff2dc7428f03529d8adc1783
Davinn354 <daviipad53@gmail.com>
2024-03-08 16:11:06 +0700

    commit ke 2

--- Bubble sort/Bubble sort.cpp
+++ Bubble sort/Bubble sort.cpp
@@ -1,23 +1,3 @@
-// Bubble sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 #include <iostream>
 #include <string>
@@ -73,5 +53,37 @@ void bubblesortArray() { //Procedur untuk mengurutkan array dengan metode bubble
     } while (pass <= n - 1); // step 5
 
 
+}
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j]; // Menampilkan array
+        if (j < n - 1) {
+            cout << " -->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jumlah pass = " << n - 1 << endl; // Menampilkan jumlah dari pass
+    cout << endl;
+    cout << endl;
+
+}
+
+int main()
+{
+    input();
+
+    bubblesortArray();
+    display();
+
+    system("pause");
+    
 }
 
